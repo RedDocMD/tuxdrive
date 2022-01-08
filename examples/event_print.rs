24619b6a8f5f8f2469aa9ae5f085bb442efe2a9b
Deep Majumder <deep.majumder2019@gmail.com>
2022-01-08 16:16:09 +0530

    New event_print example, rename script adapted

---
+++ examples/event_print.rs
@@ -0,0 +1,22 @@
+use std::{env, thread};
+use tuxdrive::watcher::{WatchEventKind, Watcher};
+
+fn main() {
+    let args = env::args().collect::<Vec<_>>();
+    const POLL_INTERVAL: u64 = 1;
+    let (mut file_watcher, event_recv) = Watcher::<{ POLL_INTERVAL }>::new().unwrap();
+    file_watcher.add_directory(&args[1], true).unwrap();
+    thread::spawn(move || file_watcher.start_polling());
+    while let Ok(ev) = event_recv.recv() {
+        println!("{}, {}", ev.path.display(), event_kind_to_string(ev.kind));
+    }
+}
+
+fn event_kind_to_string(kind: WatchEventKind) -> &'static str {
+    match kind {
+        WatchEventKind::Create => "Create",
+        WatchEventKind::Delete => "Delete",
+        WatchEventKind::Written => "Written",
+        WatchEventKind::Chmod => "Chmod",
+    }
+}
--- scripts/file_scripts/renamer.sh
+++ scripts/file_scripts/renamer.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-BASE_DIR=watch-dir
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 [base-dir]"
+    exit 1
+fi
+
+if [[ "$1" != "" ]]; then
+    BASE_DIR="$1/watch-dir"
+else
+    BASE_DIR="watch-dir"
+fi
+
+[[ -e "$BASE_DIR" ]] || mkdir "$BASE_DIR"
+
 SRC_DIR=${BASE_DIR}/from
 DEST_DIR=${BASE_DIR}/to
 
@@ -9,26 +21,28 @@ rm -rf $SRC_DIR
 rm -rf $DEST_DIR
 
 # Start the watcher
-cargo build --release
-./target/release/tuxdrive &
+OUT_FILE=$(mktemp -p /tmp "event_printXXXXXX")
+cargo build --release --example event_print
+TARGET_DIR=${CARGO_TARGET_DIR:-./target}
+"${TARGET_DIR}"/release/examples/event_print $BASE_DIR | tee "$OUT_FILE" &
 
 echo -e "\033[33;1mCreating $SRC_DIR\033[0m"
 mkdir $SRC_DIR
-sleep 6
+sleep 2
 
 FILE_CNT=10
 for i in $(seq $FILE_CNT); do
     FILE_NAME=${SRC_DIR}/file${i}
     echo -e "\033[33;1mCreating $FILE_NAME\033[0m"
-    echo "Huzzah $i" > $FILE_NAME
+    echo "Huzzah $i" > "$FILE_NAME"
     sleep 1
 done
 
-sleep 6
+sleep 2
 echo -e "\033[33;1mMoving $SRC_DIR to $DEST_DIR\033[0m"
 mv $SRC_DIR $DEST_DIR
 
-sleep 10
+sleep 3
 kill %1
 
 exit 0
